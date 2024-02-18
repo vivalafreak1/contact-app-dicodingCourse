8a55de97c80b185db885e9888d13395c4b0903d6
vivalafreak1 <arieftaufikrahman123@gmail.com>
2024-02-18 10:36:46 +0700

    Query parameter

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,23 @@
 import React from "react";
+import { useSearchParams } from "react-router-dom";
 import ContactList from "../components/ContactList";
+import SearchBar from "../components/SearchBar";
+
 import { deleteContact, getContacts } from "../utils/data";
+
+function HomePageWrapper() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const keyword = searchParams.get("keyword");
+
+  function changeSearchParams(keyword) {
+    setSearchParams({ keyword });
+  }
+
+  return (
+    <HomePage defaultKeyword={keyword} keywordChange={changeSearchParams} />
+  );
+}
 
 class HomePage extends React.Component {
   constructor(props) {
@@ -8,9 +25,11 @@ class HomePage extends React.Component {
 
     this.state = {
       contacts: getContacts(),
+      keyword: props.defaultKeyword || "",
     };
 
     this.onDeleteHandler = this.onDeleteHandler.bind(this);
+    this.onKeywordChangeHandler = this.onKeywordChangeHandler.bind(this);
   }
 
   onDeleteHandler(id) {
@@ -24,17 +43,34 @@ class HomePage extends React.Component {
     });
   }
 
+  onKeywordChangeHandler(keyword) {
+    this.setState(() => {
+      return {
+        keyword,
+      };
+    });
+
+    this.props.keywordChange(keyword);
+  }
+
   render() {
+    const contacts = this.state.contacts.filter((contact) => {
+      return contact.name
+        .toLowerCase()
+        .includes(this.state.keyword.toLowerCase());
+    });
+
     return (
       <section>
+        <SearchBar
+          keyword={this.state.keyword}
+          keywordChange={this.onKeywordChangeHandler}
+        ></SearchBar>
         <h2>Daftar Kontak</h2>
-        <ContactList
-          contacts={this.state.contacts}
-          onDelete={this.onDeleteHandler}
-        />
+        <ContactList contacts={contacts} onDelete={this.onDeleteHandler} />
       </section>
     );
   }
 }
 
-export default HomePage;
+export default HomePageWrapper;
