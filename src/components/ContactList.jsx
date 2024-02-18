a7b33911b11fa3e5850dd9a98ea076d321406e41
vivalafreak1 <arieftaufikrahman123@gmail.com>
2024-02-18 09:15:18 +0700

    PropTypes

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,20 +1,25 @@
-import React from 'react';
-import ContactItem from './ContactItem';
+import React from "react";
+import PropTypes from "prop-types";
+import ContactItem from "./ContactItem";
 
 function ContactList({ contacts, onDelete }) {
   return (
     <div className="contact-list">
-      {
-        contacts.map((contact) => (
-          <ContactItem 
+      {contacts.map((contact) => (
+        <ContactItem
           key={contact.id}
           id={contact.id}
           onDelete={onDelete}
-          {...contact} />
-        ))
-      }
+          {...contact}
+        />
+      ))}
     </div>
   );
 }
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(PropTypes.object).isRequired,
+  onDelete: PropTypes.func.isRequired,
+};
 
 export default ContactList;
