5a8d249d738cd8305c530618962d1ee3d523bee1
vivalafreak1 <arieftaufikrahman123@gmail.com>
2024-02-25 09:55:44 +0700

    Lifecycle

--- src/pages/AddPage.jsx
+++ src/pages/AddPage.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import { addContact } from "../utils/data";
+import { addContact } from "../utils/api";
 import ContactInput from "../components/ContactInput";
+import { useNavigate } from "react-router-dom";
 
 function AddPage() {
-  function onAddContactHandler(contact) {
-    addContact(contact);
+  const navigate = useNavigate();
+
+  async function onAddContactHandler(contact) {
+    await addContact(contact);
+    navigate("/");
   }
 
   return (
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import { useSearchParams } from "react-router-dom";
 import ContactList from "../components/ContactList";
 import SearchBar from "../components/SearchBar";
 
-import { deleteContact, getContacts } from "../utils/data";
+import { getContacts, deleteContact } from "../utils/api";
 
 function HomePageWrapper() {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -24,7 +24,7 @@ class HomePage extends React.Component {
     super(props);
 
     this.state = {
-      contacts: getContacts(),
+      contacts: [],
       keyword: props.defaultKeyword || "",
     };
 
@@ -32,13 +32,14 @@ class HomePage extends React.Component {
     this.onKeywordChangeHandler = this.onKeywordChangeHandler.bind(this);
   }
 
-  onDeleteHandler(id) {
-    deleteContact(id);
+  async onDeleteHandler(id) {
+    await deleteContact(id);
 
     // update the contact state from data.js
+    const { data } = await getContacts();
     this.setState(() => {
       return {
-        contacts: getContacts(),
+        contacts: data,
       };
     });
   }
@@ -51,6 +52,16 @@ class HomePage extends React.Component {
     });
 
     this.props.keywordChange(keyword);
+  }
+
+  async componentDidMount() {
+    const { data } = await getContacts();
+
+    this.setState(() => {
+      return {
+        contacts: data,
+      };
+    });
   }
 
   render() {
